e54236ff9e127a72d3a4e6a41a48a06dda3725c6
UnluckyBoy <pgiadans@outlook.com>
2022-08-07 03:53:15 -0300

    Ahora si subi el frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/layout/Home"
+import Nav from "./components/layout/Nav"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={""} element={<Nav path={""} />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
