521aa0ffa63e48c4227190ec60f83905db47895e
Pablo Giadans <PGiadans@outlook.com>
2022-08-31 10:46:58 -0300

    Se agrega el api.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var novedadesRouter = require('./routes/admin/novedades');
-//var apiRouter = require('./routes/api');
+var apiRouter = require('./routes/api');
 
 
 var app = express();
@@ -38,7 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', novedadesRouter);
-//app.use('/api', cors(), apiRouter);
+app.use('/api', cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
